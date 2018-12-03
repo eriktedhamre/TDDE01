c0321c56331df14f8746f17bd74e33c428df61ed
Erik Tedhamre <erite601@student.liu.se>
2018-12-03 16:57:55 +0100

    Soon finished with assign1

--- lab2/assign1.R
+++ lab2/assign1.R
@@ -1,8 +1,22 @@
+# 1.1
 setwd("~/TDDE01/lab2")
 data <- data.frame(read.csv("australian-crabs.csv"))
-plot(data$CL, data$RW , col = ifelse(data$sex == "Male", "blue", "red"))
+ggplot(data = data, mapping = aes(CL, RW, color = sex)) + geom_point()
 # Yes, it seems to be two pretty distinct data sets, perhaps the beginning could be tricky
-lda.model <- lda(sex ~ CL + RW, data = data)
-lda.predict <- predict(lda.model, data)
-plot(data$CL, data$RW, col = ifelse(lda.predict$x == "Male", "blue", "red"))
-
+# 1.2
+lda.model1 <- lda(sex ~ CL + RW, data = data)
+lda.predict1 <- predict(lda.model1, data)
+ggplot(data = data, mapping = aes(CL, RW, color = lda.predict1$class, shape = sex )) + geom_point()
+mcr.0.5 <- mean(lda.predict1$class != data$sex)
+# 1.3
+lda.model2 <- lda(sex ~ CL + RW, data = data, prior = c(0.1, 0.9))
+lda.predict2 <- predict(lda.model2, data)
+ggplot(data = data, mapping = aes(CL, RW, color = lda.predict2$class, shape = sex )) + geom_point()
+mcr.0.9 <- mean(lda.predict2$class != data$sex)
+# It got worse since the distribution of the data is 50/50 and not 90/10
+#1.4
+glm.model <- glm(as.factor(sex) ~ CL + RW, family = binomial, data = data)
+glm.predict <- predict(glm.model, data)
+mcr.glm <- mean(glm.predict != data$sex)
+glm.predict.0.5 <- ifelse(glm.predict > 0.5, "Female", "Male")
+table(data$sex, glm.predict.0.5)
