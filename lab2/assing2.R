a9042d60638b535022803400524d0f08e97cb4b4
Erik Tedhamre <erite601@student.liu.se>
2018-12-05 16:20:38 +0100

    Made progress with lab2

--- lab2/assing2.R
+++ lab2/assing2.R
@@ -5,15 +5,12 @@ n=dim(data.credit)[1]
 set.seed(12345) 
 id=sample(1:n, floor(n*0.5)) 
 train=data.credit[id,]
-train$Name <- NULL
 id1=setdiff(1:n, id)
 set.seed(12345) 
 id2=sample(id1, floor(n*0.25)) 
 valid=data.credit[id2,]
-valid$Name <- NULL
 id3=setdiff(id1,id2)
 test=data.credit[id3,]
-test$Name <- NULL
 
 #1.2
 tree.fit.dev <- tree(good_bad ~., data = train, split = "deviance")
@@ -32,4 +29,57 @@ pred.gini.test <- predict(tree.fit.gini, newdata = test, type="class")
 table(test$good_bad, pred.gini.test)
 
 #Deviance provides better result based on the diagonal of the confusion matrices
+
+
+fit=tree(good_bad~., data=train)
+set.seed(12345)
+max.size <- 15
+trainScore=rep(0,max.size)
+testScore=rep(0,max.size)
+for(i in 2:max.size) {
+  prunedTree=prune.tree(fit,best=i)
+  pred=predict(prunedTree, newdata=valid, type="tree")
+  trainScore[i]=deviance(prunedTree)
+  testScore[i]=deviance(pred)
+}
+plot(2:max.size, trainScore[2:max.size], type="b", col="red", ylim=c(250,400))
+points(2:max.size, testScore[2:max.size], type="b", col="blue")
+
+# 4 is very nice thank you sir
+
+finalTree=prune.tree(fit, best=4)
+# Depth 3 from plot(finalTree)
+Yfit=predict(finalTree, newdata=valid, type = "class")
+table(valid$good_bad,Yfit)
+mcr.tree <- mean(valid$good_bad != Yfit)
+#Variables actually used in tree construction:
+#[1] "savings"  "duration" "history" 
+
+
+fit.bayes=naiveBayes(good_bad~., data=train)
+Yfit.bayes.train=predict(fit.bayes, newdata=train)
+Yfit.bayes.test=predict(fit.bayes, newdata=test)
+table(Yfit.bayes.train, train$good_bad)
+table(Yfit.bayes.test, test$good_bad)
+
+# Tree is a bit better, very nice sir
+
+Yfit.tree.test=predict(finalTree, newdata=test)
+pi.vector <- seq(0.05, 0.3, 0.05)
+
+tpr.vector = double()
+fpr.vector = double()
+tree.predict <- predict(finalTree, newdata = test)
+bayes.predict <- predict(fit.bayes, newdata = test, type = "raw")
+for (pi.value in pi.vector) {
+  tree.predict.pi <- ifelse(tree.predict[,2] > pi.value, "good", "bad")
+  bayes.predict.pi <- ifelse(bayes.predict[,2] > pi.value, "good", "bad")
+  tree.table <- table(test$good_bad, tree.predict.pi)
+  bayes.table <- table(test$good_bad, bayes.predict.pi)
+  # is.finite(tree.table[5])
+  
+}
+
+
+
 
